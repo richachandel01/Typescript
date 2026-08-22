1be5ecb8bd837b85706c33f1e17e0ec95628fca6
richachandel01 <chandelricha24@gmail.com>
2026-08-22 12:30:50 +0530

    feat: add arrays and tuple examples

---
+++ day-01-basics/03-arrays-and-tuples.ts
@@ -0,0 +1,22 @@
+// Arrays
+
+let skills: string[] = [
+  "Java",
+  "JavaScript",
+  "TypeScript",
+  "React"
+];
+
+let marks: number[] = [85, 90, 78, 92];
+
+console.log("Skills:", skills);
+console.log("Marks:", marks);
+
+// Tuple
+
+let user: [string, number] = ["Richa", 22];
+
+console.log("User:", user);
+
+// Fixed structure
+// let invalidUser: [string, number] = [22, "Richa"]; // Error
